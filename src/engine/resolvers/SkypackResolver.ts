49a7e0e283cfffd71570c6e86e64cb4b260b21e8
Yousef El-Dardiry <yousefdardiry@gmail.com>
2021-04-15 19:52:39 +0200

    memoize skypackresolver

--- src/engine/resolvers/SkypackResolver.ts
+++ src/engine/resolvers/SkypackResolver.ts
@@ -1,3 +1,5 @@
+import _ from "lodash";
+
 let resolverCreated = false;
 
 function createBlob(source: string) {
@@ -35,6 +37,36 @@ export default class SkypackResolver {
     // return importShim(`https://jspm.dev/${moduleName}`);
   }
 
+  private memoizedResolveNestedModule = _.memoize((moduleName: string) => {
+    if (moduleName.startsWith("/-/")) {
+      // skypack, e.g.: https://cdn.skypack.dev/-/react-sortable-tree@v2.8.0-nFKv1Y1I3NJ65IUkUWwI/dist=es2020,mode=imports/unoptimized/dist/index.cjs.js
+      // TODO: should also pass version identifier (@xx)
+      let matches = moduleName.match(/^\/-\/(.+)@/);
+      if (!matches || !matches[1]) {
+        throw new Error("couldn't match url");
+      }
+      moduleName = matches[1];
+    }
+
+    const module =
+      this.resolveNestedModule && this.resolveNestedModule(moduleName);
+
+    if (module) {
+      const safeName = moduleName.replaceAll(/[^a-zA-Z0-9_]/g, "$");
+      (window as any)["__typecell_" + safeName] = module;
+      const list = Object.keys(module).filter((key) => key !== "default");
+
+      const url = createBlob(`
+      const ${safeName} = window.__typecell_${safeName};
+      const { ${list.join(",")} } = ${safeName};
+      export { ${list.join(",")} };
+      export default ${safeName};
+    `);
+      return url;
+    }
+    return undefined;
+  });
+
   private async getImportShim() {
     if (this.importShim) {
       return this.importShim;
@@ -57,39 +89,14 @@ export default class SkypackResolver {
     // @ts-ignore
     const resolve = this.importShim.resolve;
     this.importShim.resolve = (id: string, parent: string) => {
-      let moduleName = id;
       //   if (moduleName.startsWith("/npm:")) {
       //     // jspm, e.g.: https://ga.jspm.io/npm:es-module-shims@0.10.1/dist/es-module-shims.min.js
       //   } else
 
-      if (moduleName.startsWith("/-/")) {
-        // skypack, e.g.: https://cdn.skypack.dev/-/react-sortable-tree@v2.8.0-nFKv1Y1I3NJ65IUkUWwI/dist=es2020,mode=imports/unoptimized/dist/index.cjs.js
-        // TODO: should also pass version identifier (@xx)
-        let matches = moduleName.match(/^\/-\/(.+)@/);
-        if (!matches || !matches[1]) {
-          throw new Error("couldn't match url");
-        }
-        moduleName = matches[1];
+      let ret = this.memoizedResolveNestedModule(id);
+      if (!ret) {
+        ret = resolve(id, parent);
       }
-
-      const module =
-        this.resolveNestedModule && this.resolveNestedModule(moduleName);
-
-      if (module) {
-        const safeName = moduleName.replaceAll(/[^a-zA-Z0-9_]/g, "$");
-        (window as any)["__typecell_" + safeName] = module;
-        const list = Object.keys(module).filter((key) => key !== "default");
-
-        const url = createBlob(`
-        const ${safeName} = window.__typecell_${safeName};
-        const { ${list.join(",")} } = ${safeName};
-        export { ${list.join(",")} };
-        export default ${safeName};
-      `);
-        return url;
-      }
-
-      const ret = resolve(id, parent);
       return ret;
     };
 
