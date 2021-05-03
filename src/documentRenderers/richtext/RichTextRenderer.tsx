6e7ae15d07f332c0c6fc8976b3572a44a027ee4f
Matthew Lipski <matthewlipski@gmail.com>
2021-05-03 14:35:10 +0200

    Revert "Added files for drag handles (non-functioal)"
    This reverts commit 3b627020a04a6664ff93ce87cff0826245c6b86b.

--- src/documentRenderers/richtext/RichTextRenderer.tsx
+++ src/documentRenderers/richtext/RichTextRenderer.tsx
@@ -1,24 +1,16 @@
 import Collaboration from "@tiptap/extension-collaboration";
 import CollaborationCursor from "@tiptap/extension-collaboration-cursor";
-import { useEditor, EditorContent, Extension } from "@tiptap/react";
+import { useEditor, EditorContent } from "@tiptap/react";
 import { defaultExtensions } from "@tiptap/starter-kit";
 import { observer } from "mobx-react-lite";
 import React from "react";
 
 import { DocumentResource } from "../../store/DocumentResource";
 import TypeCellNode from "./extensions/typecellnode";
-import DraggableNode from "./DraggableNode";
-import { Paragraph } from "@tiptap/extension-paragraph"
 
 type Props = {
   document: DocumentResource;
 };
-
-const Drag = Paragraph.extend({
-  draggable: true,
-})
-
-
 const RichText: React.FC<Props> = observer((props) => {
   const editor = useEditor({
     onUpdate: ({ editor }) => {
@@ -26,7 +18,6 @@ const RichText: React.FC<Props> = observer((props) => {
     },
     extensions: [
       ...defaultExtensions(),
-      // Drag,
 
       CollaborationCursor.configure({
         provider: props.document.webrtcProvider,
@@ -36,20 +27,8 @@ const RichText: React.FC<Props> = observer((props) => {
         fragment: props.document.data,
       }),
       TypeCellNode,
-      DraggableNode,
     ],
-    content: `
-        <div>
-          <div data-type="draggable-item">1</div>
-          <div data-type="draggable-item">2</div>
-          <div data-type="draggable-item">3</div>
-        </div>
-<!--        <div>-->
-<!--            <p>Drag 1</p>-->
-<!--            <p>Drag 2</p>-->
-<!--            <p>Drag 3</p>-->
-<!--        </div>-->
-    `,
+    content: "This text is in a TipTap editor, feel free to change it. Live collaboration is also enabled.",
   });
 
   return (
