16f891c5ace728e45daa20299e6f01caf3fd4c8d
Matthew Lipski <matthewlipski@gmail.com>
2021-05-11 11:49:16 +0200

    Refactor: Add Prettier and reformat

--- src/documentRenderers/richtext/RichTextRenderer.tsx
+++ src/documentRenderers/richtext/RichTextRenderer.tsx
@@ -40,14 +40,14 @@ const RichTextRenderer: React.FC<Props> = observer((props) => {
       },
     },
 
-    content: `
-      <div>
-        <p>Item 1</p>
-        <p>Item 2</p>
-        <p>Item 3</p>
-        <p>Item 4</p>
-      </div>
-      `,
+    // content: `
+    //   <div>
+    //     <p>Item 1</p>
+    //     <p>Item 2</p>
+    //     <p>Item 3</p>
+    //     <p>Item 4</p>
+    //   </div>
+    //   `,
   });
 
   return (
