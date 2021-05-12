fc71f86bafafdd45a2d096b8209f032426992779
Nikko <N.A.Zhlebinkov@student.tudelft.nl>
2021-05-12 10:30:13 +0200

    Test: does ci catch style warnings

--- src/documentRenderers/richtext/extensions/typecellnode/TypeCellComponent.tsx
+++ src/documentRenderers/richtext/extensions/typecellnode/TypeCellComponent.tsx
@@ -2,11 +2,14 @@ import { NodeViewContent, NodeViewWrapper } from "@tiptap/react";
 import React from "react";
 
 export default function TypeCellComponent(props: any) {
+  let x = "String";
+  let y = "false";
   return (
     <NodeViewWrapper as="div" className="react-component">
       <div>
         {/* <div {...attributes} className={classNames.root}> */}
         <div style={{ background: "gray" }}>
+          {x == y}
           <div>hello</div>
           <NodeViewContent as="div" />
         </div>
