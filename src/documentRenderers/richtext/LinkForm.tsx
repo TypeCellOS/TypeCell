c6a12c750b91552c10e25bd33cd45bc89ce5e865
Emre Agca <emregca@gmail.com>
2021-05-15 14:52:39 +0200

    WIP: Add matching css and react-icon

--- src/documentRenderers/richtext/LinkForm.tsx
+++ src/documentRenderers/richtext/LinkForm.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { AiOutlineLink } from 'react-icons/ai';
+import { IconContext } from "react-icons";
 
 import "./LinkForm.css";
 
@@ -19,8 +21,13 @@ const LinkForm: React.FC = (props) => {
   return (
     <div className={`link-menu`}>
       <form onSubmit={handleSubmit}>
-        <input type="text" onChange={handleChange}/>
-        <input type="submit" value=">"/>
+        <IconContext.Provider value={{ className: "link-icon" }}>
+          <div className={'link-icon-container'} >
+            <AiOutlineLink />
+          </div>
+        </IconContext.Provider>
+        <input className={'input-field'} type="text" onChange={handleChange}/>
+        <input className={'submit-button'} type="submit" value=">"/>
       </form>
     </div>
   );
