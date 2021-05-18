4ba43d7f315b84b169cb0a349c9563fa16a06c51
Emre Agca <emregca@gmail.com>
2021-05-18 14:34:12 +0200

    Revert "WIP: Experimental implementation"
    This reverts commit c41d2987f68f5cee27ebda64f982820ff2364b8a. Postpone
    implementation of url showing in the link text field when highlighting an
    existing link

--- src/documentRenderers/richtext/InlineMenu.tsx
+++ src/documentRenderers/richtext/InlineMenu.tsx
@@ -1,69 +1,69 @@
 import { BubbleMenu, Editor } from "@tiptap/react";
-import React, { useRef } from "react";
+import React from "react";
 import styles from "./InlineMenu.module.css";
 import Tippy from "@tippyjs/react";
 import LinkForm from "./LinkForm"
 
 type InlineMenuProps = { editor: Editor };
 
-const InlineMenu: React.FC<InlineMenuProps> = (props: InlineMenuProps) =>{
-
-  const linkRef = useRef();
-
-  return (
-    <BubbleMenu className={styles.BubbleMenu} editor={props.editor}>
-      <button
-        onClick={() => props.editor.chain().focus().toggleBold().run()}
-        className={props.editor.isActive("bold") ? styles.isActive : ""}>
-        Bold
-      </button>
-      <button
-        onClick={() => props.editor.chain().focus().toggleItalic().run()}
-        className={
-          props.editor.isActive("italic") ? styles.isActive : ""
-        }>
-        Italic
-      </button>
-      <button
-        onClick={() => props.editor.chain().focus().toggleStrike().run()}
-        className={
-          props.editor.isActive("strike") ? styles.isActive : ""
-        }>
-        Strike
-      </button>
-      <button
-        onClick={() => props.editor.chain().focus().toggleCode().run()}
-        className={props.editor.isActive("code") ? styles.isActive : ""}>
-        Code
-      </button>
-      <button
-        onClick={() =>
-          props.editor.chain().focus().toggleUnderline().run()
-        }
-        className={
-          props.editor.isActive("underline") ? styles.isActive : ""
-        }>
-        Underline
-      </button>
-      <Tippy
-        // @ts-ignore
-        content={<LinkForm editor={props.editor} ref={linkRef}></LinkForm>}
-        trigger={"click"}
-        placement={"top"}
-        interactive={true}>
+class InlineMenu extends React.Component<InlineMenuProps> {
+  render() {
+    return (
+      <BubbleMenu className={styles.BubbleMenu} editor={this.props.editor}>
+        <button
+          onClick={() => this.props.editor.chain().focus().toggleBold().run()}
+          className={this.props.editor.isActive("bold") ? styles.isActive : ""}>
+          Bold
+        </button>
+        <button
+          onClick={() => this.props.editor.chain().focus().toggleItalic().run()}
+          className={
+            this.props.editor.isActive("italic") ? styles.isActive : ""
+          }>
+          Italic
+        </button>
+        <button
+          onClick={() => this.props.editor.chain().focus().toggleStrike().run()}
+          className={
+            this.props.editor.isActive("strike") ? styles.isActive : ""
+          }>
+          Strike
+        </button>
+        <button
+          onClick={() => this.props.editor.chain().focus().toggleCode().run()}
+          className={this.props.editor.isActive("code") ? styles.isActive : ""}>
+          Code
+        </button>
         <button
           onClick={() =>
-            // @ts-ignore
-            linkRef.current.toggleExpansion()
+            this.props.editor.chain().focus().toggleUnderline().run()
           }
           className={
-            props.editor.isActive("link") ? styles.isActive : ""
+            this.props.editor.isActive("underline") ? styles.isActive : ""
           }>
-          Link
+          Underline
         </button>
-      </Tippy>
-    </BubbleMenu>
-  );
+        <Tippy
+          // @ts-ignore
+          content={<LinkForm editor={this.props.editor}></LinkForm>}
+          trigger={"click"}
+          placement={"top"}
+          interactive={true}>
+          <button
+            /*
+            onClick={() =>
+              this.props.editor.chain().focus().toggleUnderline().run()
+            }
+            */
+            className={
+              this.props.editor.isActive("link") ? styles.isActive : ""
+            }>
+            Link
+          </button>
+        </Tippy>
+      </BubbleMenu>
+    );
+  }
 }
 
 export default InlineMenu;
