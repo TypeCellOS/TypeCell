c74f905f45ef6bd45dba875976726240794086f0
Chong Zhao <c.zhao-2@student.tudelft.nl>
2021-05-21 16:46:35 +0200

    WIP: Fix the bug induced by ToggleHeader
    Find the position of parent node (td) and select that node to ensure successful toggling.

--- src/documentRenderers/richtext/TableMenu.tsx
+++ src/documentRenderers/richtext/TableMenu.tsx
@@ -111,9 +111,30 @@ class TableMenu extends React.Component<TableMenuProps> {
             <button
               className={tableStyles.tableMenuOption}
               disabled={!this.props.editor.can().toggleHeaderRow()}
-              onClick={() =>
-                this.props.editor.chain().focus().toggleHeaderRow().run()
-              }>
+              onClick={() => {
+                // current depth where the text in a paragraph block is located
+                const currentDepth = resolvedPos.depth;
+
+                // We cannot acquire where the position of the start of this block is
+                // So budge the cursor little by little until the depth changes
+                // Which means this cursor position is right before <p>
+                let shift;
+                for (shift = 0; ; shift++) {
+                  const newPos = this.props.editor.state.doc.resolve(
+                    this.props.editor.state.selection.from - shift
+                  );
+                  if (newPos.depth !== currentDepth) {
+                    break;
+                  }
+                }
+
+                // This position (<td>|<p>text...</p>) will be used for selection of a TableCell selection
+                this.props.editor.commands.setNodeSelection(
+                  this.props.editor.state.selection.from - shift
+                );
+
+                this.props.editor.chain().focus().toggleHeaderRow().run();
+              }}>
               <div className={tableStyles.optionIcon}>
                 <CgFormatHeading></CgFormatHeading>
               </div>
