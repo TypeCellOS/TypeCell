a2f9d4b2c1731de6255bfe8ee173e4bf5725fa5d
Emre Agca <emregca@gmail.com>
2021-05-24 17:47:18 +0200

    Apply PR suggestions, comment, further polish

--- src/documentRenderers/richtext/LinkForm.tsx
+++ src/documentRenderers/richtext/LinkForm.tsx
@@ -1,14 +1,25 @@
 import { Editor } from "@tiptap/react";
 import React, { useEffect, useState } from "react";
-import { AiOutlineLink } from 'react-icons/ai';
 import styles from "./LinkForm.module.css";
 
 export type LinkFormProps = { editor: Editor };
 
 const LinkForm: React.FC<LinkFormProps> = (props: LinkFormProps) => {
 
+  /**
+   * The input form is a controlled component and uses the url state.
+   * 
+   * The "value" attribute of the input form is set to this state,
+   * and it gets updated throughout the functions in this file.
+   */
   const [url, setUrl] = useState("");
 
+  /**
+   * Updates the url state in the background(when link menu is out of focus)
+   * 
+   * If the selection has an active link mark, removes the first two characters:
+   * "//", and updates url state. Gets called when the url state changes
+   */
   useEffect(() => {
     if(props.editor.isFocused) {
       if(props.editor.isActive("link")) {
@@ -19,8 +30,14 @@ const LinkForm: React.FC<LinkFormProps> = (props: LinkFormProps) => {
     }
   });
 
-
-
+  /**
+   * Handles form submit
+   * 
+   * Called when the submit button is clicked. Will remove the link mark
+   * if the url state is empty, and set a link if its not. The url that will
+   * be set is prepended with "//" to force the browser open it as an absolute
+   * path.
+   */
   const handleSubmit = (e: any) => {
 
     const absoluteUrl = "//" + url;
@@ -34,6 +51,9 @@ const LinkForm: React.FC<LinkFormProps> = (props: LinkFormProps) => {
     }
   }
 
+  /**
+   * Updates the url state
+   */
   const handleChange = (e: any) => {
     setUrl(e.target.value);
   }
@@ -41,11 +61,6 @@ const LinkForm: React.FC<LinkFormProps> = (props: LinkFormProps) => {
   return (
     <div className={styles.formContainer}>
       <form onSubmit={handleSubmit}>
-        {/*
-        <div className={styles.linkIconContainer} >
-          <AiOutlineLink />
-        </div>
-        */}
         <input autoFocus className={styles.inputField} type="text" placeholder="URL" value={url} onChange={handleChange}/>
         <input className={styles.submitButton} type="submit" value="OK"/>
       </form>
--- src/documentRenderers/richtext/extensions/marks/CustomLink.tsx
+++ src/documentRenderers/richtext/extensions/marks/CustomLink.tsx
@@ -3,66 +3,39 @@ import { Plugin, PluginKey }  from 'prosemirror-state'
 
 export const pasteRegexExact = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z]{2,}\b(?:[-a-zA-Z0-9@:%._+~#=?!&/]*)(?:[-a-zA-Z0-9@:%._+~#=?!&/]*)$/gi
 
+/**
+ * This extends the default link mark
+ * 
+ * The two plugins are taken from the link mark library, with its name changed.
+ * The default option for handleOnClick is disabled in RichTextRenderer, 
+ * and this one is to be used instead to be able to extend the behaviour.
+ */
 export const CustomLink = Link.extend({
     addProseMirrorPlugins() {
     const plugins = []
 
     plugins.push(
-    new Plugin({
-        key: new PluginKey('handleClickLink'),
-        props: {
-        handleClick: (view, pos, event) => {
-            const attrs = this.editor.getAttributes('link')
-            console.log("event is, ", event.type);
-            console.log("detail is, ", event.detail);
-            const link = (event.target as HTMLElement)?.closest('a')
-
-            if (link && attrs.href) {
-            window.open(attrs.href, attrs.target)
-
-            return true
-            }
-
-            return false
-        },
-        },
-    }),
-    )
-
-    if (this.options.linkOnPaste) {
-      plugins.push(
-        new Plugin({
-          key: new PluginKey('handlePasteLink'),
+      new Plugin({
+          key: new PluginKey('handleClickLink'),
           props: {
-            handlePaste: (view, event, slice) => {
-              const { state } = view
-              const { selection } = state
-              const { empty } = selection
+          handleClick: (view, pos, event) => {
+              const attrs = this.editor.getAttributes('link')
+              const link = (event.target as HTMLElement)?.closest('a')
 
-              if (empty) {
-                return false
-              }
-
-              let textContent = ''
-
-              slice.content.forEach(node => {
-                textContent += node.textContent
-              })
-
-              if (!textContent || !textContent.match(pasteRegexExact)) {
-                return false
-              }
-
-              this.editor.commands.setMark(this.type, {
-                href: textContent,
-              })
+              if (link && attrs.href) {
+              window.open(attrs.href, attrs.target)
 
               return true
-            },
+              }
+
+              return false
           },
-        }),
-      )
-    }
+          },
+      }),
+    )
+
+    // To create a new plugin, add it to plugins list. See default link mark.
+
 
     return plugins
     },
