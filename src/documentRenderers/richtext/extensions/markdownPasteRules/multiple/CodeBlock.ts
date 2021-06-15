1b89e53593c693aafaea6790cd0c40934d54a72d
Yousef El-Dardiry <yousefdardiry@gmail.com>
2021-06-15 10:35:48 +0200

    remove lowlight fix

--- src/documentRenderers/richtext/extensions/markdownPasteRules/multiple/CodeBlock.ts
+++ src/documentRenderers/richtext/extensions/markdownPasteRules/multiple/CodeBlock.ts
@@ -1,4 +1,4 @@
-import CodeBlockLowlight from "../../lowlight-highlight";
+import CodeBlockLowlight from "@tiptap/extension-code-block-lowlight";
 import lowlight from "lowlight";
 import multipleLineMarkdownRuleBuilder from "./markdownMultipleLines";
 import "./CodeBlock.module.css";
