5103d0c4a57c9ba1bb8cee9971982b667f726900
Yousef El-Dardiry <yousefdardiry@gmail.com>
2021-06-15 11:01:08 +0200

    upgrade tiptap

--- src/documentRenderers/richtext/extensions/slashcommand/SlashCommandExtension.ts
+++ src/documentRenderers/richtext/extensions/slashcommand/SlashCommandExtension.ts
@@ -1,12 +1,33 @@
-import { Extension } from "@tiptap/core";
+import { Extension, Range } from "@tiptap/core";
 import { Selection } from "prosemirror-state";
 import defaultCommands from "./defaultCommands";
 import { SlashCommand } from "./SlashCommand";
 import { SuggestionPlugin } from "../../prosemirrorPlugins/suggestions/SuggestionPlugin";
+import { Node } from "prosemirror-model";
 
 export type SlashCommandOptions = {
   commands: { [key: string]: SlashCommand };
 };
+
+declare module "@tiptap/core" {
+  interface Commands<ReturnType> {
+    replaceRangeCustom: {
+      /**
+       * Command for replacing a range with a node.
+       *
+       * This command tries to put the cursor at the start of the newly created node,
+       * such that the user can start typing in the new node immediately.
+       *
+       * **Only use this command works best for inserting nodes that contain editable text.**
+       *
+       * @param range the range
+       * @param node the prosemirror node
+       * @returns true iff the command succeeded
+       */
+      replaceRangeCustom: (range: Range, node: Node) => ReturnType;
+    };
+  }
+}
 
 export const SlashCommandExtension = Extension.create<SlashCommandOptions>({
   name: "slash-command",
