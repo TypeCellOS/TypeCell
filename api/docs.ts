7420418446af80b3af96939e2ea8787180c114ea
Yousef El-Dardiry <yousefdardiry@gmail.com>
2021-10-02 12:04:58 +0200

    serve docs

--- api/docs.ts
+++ api/docs.ts
@@ -1,7 +1,8 @@
 import * as fs from "fs";
+import * as path from "path";
 
 export default function (req: any, res: any) {
-  let dirfiles = fs.readdirSync(__dirname);
+  let dirfiles = fs.readdirSync(path.join(__dirname, "docs"));
   let rootfiles = fs.readdirSync(".");
   res.json({
     dirfiles,
