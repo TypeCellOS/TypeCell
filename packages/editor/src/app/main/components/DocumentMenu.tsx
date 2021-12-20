1d1ba0b5a253bbf25f076b057edf086a0ec35f25
Niklas Kors <me@niklaskors.com>
2021-12-20 15:09:15 +0100

    Move fork alert to document menu

--- packages/editor/src/app/main/components/DocumentMenu.tsx
+++ packages/editor/src/app/main/components/DocumentMenu.tsx
@@ -15,6 +15,9 @@ import ShareIcon from "@atlaskit/icon/glyph/share";
 import DropdownMenu, { DropdownItem } from "@atlaskit/dropdown-menu";
 import { openAsMarkdown } from "../../../integrations/markdown/export";
 import { DocumentResource } from "../../../store/DocumentResource";
+import { BaseResource } from "../../../store/BaseResource";
+import { UnreachableCaseError } from "../../../util/UnreachableCaseError";
+import EditorWarningIcon from "@atlaskit/icon/glyph/editor/warning";
 
 type Props = {
   document: DocumentResource;
@@ -22,40 +25,61 @@ type Props = {
 
 export const DocumentMenu: React.FC<Props> = observer((props) => {
   const navigationStore = getStoreService().navigationStore;
-  //   const sessionStore = getStoreService().sessionStore;
+  const sessionStore = getStoreService().sessionStore;
 
-  //   const forkAction = sessionStore.isLoggedIn ? (
-  //     <a
-  //       href=""
-  //       onClick={async (e) => {
-  //         e.preventDefault();
-  //         if (!navigationStore.currentDocument) {
-  //           throw new Error("unexpected, forking without currentDocument");
-  //         }
-  //         const result = await navigationStore.currentDocument.fork();
-  //         if (result instanceof BaseResource) {
-  //           navigationStore.navigateToDocument(result);
-  //         } else {
-  //           if (result.status !== "error") {
-  //             throw new UnreachableCaseError(result.status);
-  //           }
-  //           throw new Error("error while forking");
-  //         }
-  //         return false;
-  //       }}>
-  //       save a copy
-  //     </a>
-  //   ) : (
-  //     <a
-  //       href=""
-  //       onClick={(e) => {
-  //         navigationStore.showLoginScreen();
-  //         e.preventDefault();
-  //         return false;
-  //       }}>
-  //       sign in to save a copy
-  //     </a>
-  //   );
+  const getForkAlert = function () {
+    const forkAction = sessionStore.isLoggedIn ? (
+      <a
+        href=""
+        onClick={async (e) => {
+          e.preventDefault();
+          if (!navigationStore.currentDocument) {
+            throw new Error("unexpected, forking without currentDocument");
+          }
+          const result = await navigationStore.currentDocument.fork();
+          if (result instanceof BaseResource) {
+            navigationStore.navigateToDocument(result);
+          } else {
+            if (result.status !== "error") {
+              throw new UnreachableCaseError(result.status);
+            }
+            throw new Error("error while forking");
+          }
+          return false;
+        }}>
+        <span>save a copy</span>
+      </a>
+    ) : (
+      <a
+        href=""
+        onClick={(e) => {
+          navigationStore.showLoginScreen();
+          e.preventDefault();
+          return false;
+        }}>
+        <span>sign in to save a copy</span>
+      </a>
+    );
+
+    return (
+      navigationStore.currentDocument?.needsFork && (
+        <div className={styles.fork_alert}>
+          <EditorWarningIcon size="small" label="Warning"></EditorWarningIcon>
+          Usaved changes ({forkAction}/
+          <a
+            href=""
+            onClick={(e) => {
+              navigationStore.currentDocument?.revert();
+              e.preventDefault();
+              return false;
+            }}>
+            <span>revert</span>
+          </a>
+          )
+        </div>
+      )
+    );
+  };
 
   const getBreadcrumbItems = function (docConnection: DocConnection) {
     const { identifier } = docConnection;
@@ -95,11 +119,12 @@ export const DocumentMenu: React.FC<Props> = observer((props) => {
 
   return (
     <nav className={styles.menu}>
-      <div>
-        <Breadcrumbs>
-          {getBreadcrumbItems(navigationStore.currentDocument!)}
-        </Breadcrumbs>
-      </div>
+      <Breadcrumbs>
+        {getBreadcrumbItems(navigationStore.currentDocument!)}
+      </Breadcrumbs>
+
+      {getForkAlert()}
+
       <aside className={styles.actions}>
         <ul>
           <li className={styles.item}>
@@ -108,20 +133,26 @@ export const DocumentMenu: React.FC<Props> = observer((props) => {
             </div>
             <span>Share</span>
           </li>
-          <li className={styles.separator}></li>
-          <li className={styles.options}>
-            <DropdownMenu
-              shouldFlip
-              trigger={
-                <MoreVerticalIcon primaryColor="#bdbdbd" label="Options" />
-              }
-              position="bottom right">
-              <DropdownItem onClick={() => openAsMarkdown(props.document)}>
-                Export as markdown
-              </DropdownItem>
-              {/* TODO <DropdownItem>Change permissions</DropdownItem> */}
-            </DropdownMenu>
-          </li>
+          {props.document.type === "!notebook" ? (
+            <>
+              <li className={styles.separator}></li>
+              <li className={styles.options}>
+                <DropdownMenu
+                  shouldFlip
+                  trigger={
+                    <MoreVerticalIcon primaryColor="#bdbdbd" label="Options" />
+                  }
+                  position="bottom right">
+                  <DropdownItem onClick={() => openAsMarkdown(props.document)}>
+                    Export as markdown
+                  </DropdownItem>
+                  {/* TODO <DropdownItem>Change permissions</DropdownItem> */}
+                </DropdownMenu>
+              </li>
+            </>
+          ) : (
+            ""
+          )}
         </ul>
       </aside>
     </nav>
