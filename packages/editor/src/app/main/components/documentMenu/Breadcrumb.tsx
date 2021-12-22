576ebc15c0a838b099c55d8e38abf07c7a9f5be1
Yousef El-Dardiry <yousefdardiry@gmail.com>
2021-12-22 15:12:33 +0100

    improving breadcrumbs

--- packages/editor/src/app/main/components/documentMenu/Breadcrumb.tsx
+++ packages/editor/src/app/main/components/documentMenu/Breadcrumb.tsx
@@ -1,5 +1,6 @@
 import Breadcrumbs, { BreadcrumbsItem } from "@atlaskit/breadcrumbs";
 import { observer } from "mobx-react-lite";
+import { VscFile, VscGithub, VscGlobe } from "react-icons/vsc";
 import { FileIdentifier } from "../../../../identifiers/FileIdentifier";
 import { GithubIdentifier } from "../../../../identifiers/GithubIdentifier";
 import { HttpsIdentifier } from "../../../../identifiers/HttpsIdentifier";
@@ -11,36 +12,77 @@ const getBreadcrumbItems = function (navigationStore: NavigationStore) {
   const { identifier } = navigationStore.currentDocument!;
   const { path } = identifier.uri;
 
-  if (
-    identifier instanceof FileIdentifier ||
-    identifier instanceof GithubIdentifier ||
-    identifier instanceof HttpsIdentifier
-  ) {
+  const items: JSX.Element[] = [];
+
+  if (identifier instanceof FileIdentifier) {
+    debugger;
     // Show path as single item
-    return (
+    items.push(
       <BreadcrumbsItem
-        text={path} // Replace default component so it doesn't render as a link
-        component={() => <span>{path}</span>}
+        iconBefore={<VscFile />}
+        text={path}
+        onClick={() => (identifier.subPath = "")}
+      />
+    );
+  } else if (identifier instanceof GithubIdentifier) {
+    // Show path as single item
+    items.push(
+      <BreadcrumbsItem
+        iconBefore={<VscGithub style={{ marginRight: 5 }} />}
+        href=""
+        onClick={() => (identifier.subPath = "")}
+        text={path}
+      />
+    );
+  } else if (identifier instanceof HttpsIdentifier) {
+    // Show path as single item
+    items.push(
+      <BreadcrumbsItem
+        iconBefore={<VscGlobe />}
+        href=""
+        text={path}
+        onClick={() => (identifier.subPath = "")}
       />
     );
   } else if (identifier instanceof MatrixIdentifier) {
-    return (
-      <>
+    items.push(
+      <BreadcrumbsItem
+        href=""
+        text={identifier.owner}
+        onClick={() => {
+          navigationStore.showProfilePage(identifier.owner);
+        }}
+      />,
+      <BreadcrumbsItem
+        text={identifier.document}
+        // Replace default component so it doesn't render as a link
+        component={() => <span>{identifier.document}</span>}
+      />
+    );
+  } else {
+    throw new Error("unsupported identifier");
+  }
+
+  if (identifier.subPath) {
+    const parts = identifier.subPath.split("/");
+    const subItems: JSX.Element[] = [];
+    while (parts.length) {
+      const link = parts.join("/");
+      const part = parts.pop()!;
+      subItems.push(
         <BreadcrumbsItem
+          text={part}
           href=""
-          text={identifier.owner}
           onClick={() => {
-            navigationStore.showProfilePage(identifier.owner);
+            identifier.subPath = link;
           }}
         />
-        <BreadcrumbsItem
-          text={identifier.document}
-          // Replace default component so it doesn't render as a link
-          component={() => <span>{identifier.document}</span>}
-        />
-      </>
-    );
+      );
+    }
+    subItems.reverse();
+    items.push.apply(items, subItems);
   }
+  return items;
 };
 
 export const Breadcrumb: React.FC<{}> = observer((props) => {
