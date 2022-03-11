fe7960a6ab1d4182d40c771a323b099763204132
Yousef <yousefdardiry@gmail.com>
2022-03-11 08:34:06 -0400

    Remove richtext (#306)
    * upgrade vscode lib
    
    * remove richtext related code

--- packages/editor/src/app/documentRenderers/richtext/extensions/mentions/index.ts
+++
@@ -1,7 +0,0 @@
-import { MentionsExtension } from "./MentionsExtension";
-
-import { Mention, MentionType } from "./Mention";
-
-export { Mention, MentionType };
-
-export default MentionsExtension;
