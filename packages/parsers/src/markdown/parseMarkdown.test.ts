879fe10d5a8aa59e59b7dbf2b0522cba011adc6a
yousefed <yousefdardiry@gmail.com>
2022-11-11 06:39:30 +0100

    fix tests

--- packages/parsers/src/markdown/parseMarkdown.test.ts
+++ packages/parsers/src/markdown/parseMarkdown.test.ts
@@ -1,9 +1,15 @@
 import * as glob from "fast-glob";
 import * as fs from "fs";
+import * as path from "path";
 import { describe, expect, it } from "vitest";
 import { markdownToDocument } from "./parseMarkdown";
+
+const ROOT_DIR = path.resolve(__dirname, "../../../../");
+
 describe("parseMarkdown", () => {
-  const mds = glob.sync("tests/data/markdown/**/*.md");
+  const mds = glob.sync(
+    path.join(ROOT_DIR, "shared/test-data/markdown/**/*.md")
+  );
   mds.forEach((md) => {
     it("converts " + md, () => {
       const data = fs.readFileSync(md, "utf-8");
