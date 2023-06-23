3e5cfb9ca024e78e1c7a5bb65e061d0cc1310940
yousefed <yousefdardiry@gmail.com>
2023-06-23 09:22:58 +0200

    fix build

--- packages/server/src/test/setup.ts
+++ packages/server/src/test/setup.ts
@@ -1,6 +1,7 @@
 import { startSupabase } from "../supabase/test/supabaseCLIUtil";
 
 export default async function () {
+  // in CI, supabase is already started
   if (!process.env.CI) {
     await startSupabase();
   }
