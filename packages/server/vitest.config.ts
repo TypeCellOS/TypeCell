0cbdd4cc80def685f43e42c289c6e834fcd2dc25
yousefed <yousefdardiry@gmail.com>
2023-07-10 21:00:13 +0200

    test vitest workspaces

--- packages/server/vitest.config.ts
+++ packages/server/vitest.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    globalSetup: ["./src/test/setup.ts"],
+    // globalSetup: ["./packages/server/src/test/setup.ts"],
   },
 });
