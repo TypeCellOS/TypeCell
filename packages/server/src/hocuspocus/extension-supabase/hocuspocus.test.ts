563d7667d7e023347f8e952adde244fe9c95a1ba
yousefed <yousefdardiry@gmail.com>
2023-07-11 17:32:21 +0200

    cleanup

--- packages/server/src/hocuspocus/extension-supabase/hocuspocus.test.ts
+++ packages/server/src/hocuspocus/extension-supabase/hocuspocus.test.ts
@@ -21,56 +21,6 @@ beforeAll(async () => {
   });
   await server.listen(0); // 0 for random port
 });
-/*
-it("should sync user data via yjs", async () => {
-  const ydoc = new Y.Doc();
-
-  let pResolve = () => {};
-  const p = new Promise<void>((resolve) => {
-    pResolve = resolve;
-  });
-  const wsProvider = new HocuspocusProviderWebsocket({
-    url: "ws://localhost:1234",
-    WebSocketPolyfill: ws,
-  });
-
-  const provider = new HocuspocusProvider({
-    name: generateId(),
-    document: ydoc,
-    token: alice.session?.access_token,
-    onAuthenticated: () => {
-      console.log("onAuthenticated");
-    },
-    onAuthenticationFailed(data) {
-      console.log("onAuthenticationFailed", data);
-    },
-    onStatus: (data) => {
-      console.log("onStatus", data);
-    },
-    onSynced: () => {
-      console.log("onSynced");
-      pResolve();
-    },
-    onConnect() {
-      console.log("onConnect");
-    },
-    websocketProvider: wsProvider,
-    broadcast: false,
-  });
-
-  provider.on("awarenessUpdate", () => {
-    console.log("awareness");
-  });
-  await p;
-  ydoc.getMap("hello").set("world", "hello");
-  await new Promise((resolve) => setTimeout(resolve, 3000));
-  console.log("next change");
-  ydoc.getMap("hello").set("world", "hello2");
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-  // await provider.connect();
-  //   await p;
-  //   provider.on("synced", () => {});
-});*/
 
 describe("SupabaseHocuspocus", () => {
   let alice: Awaited<ReturnType<typeof createRandomUser>>;
