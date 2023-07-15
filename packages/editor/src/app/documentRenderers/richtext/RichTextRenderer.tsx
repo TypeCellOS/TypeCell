5e849bffccd98f362d565741fe9e83008ff73fb8
yousefed <yousefdardiry@gmail.com>
2023-07-15 13:46:43 +0200

    fix build

--- packages/editor/src/app/documentRenderers/richtext/RichTextRenderer.tsx
+++ packages/editor/src/app/documentRenderers/richtext/RichTextRenderer.tsx
@@ -1,10 +1,8 @@
 import { observer } from "mobx-react-lite";
 import React from "react";
 // import LocalExecutionHost from "../../../runtime/executor/executionHosts/local/LocalExecutionHost"
-import { EditorContent, useEditor } from "@blocknote/core";
 import "@blocknote/core/style.css";
-import Collaboration from "@tiptap/extension-collaboration";
-import CollaborationCursor from "@tiptap/extension-collaboration-cursor";
+import { BlockNoteView, useBlockNote } from "@blocknote/react";
 import ReactDOM from "react-dom";
 import { DocumentResource } from "../../../store/DocumentResource";
 import { SessionStore } from "../../../store/local/SessionStore";
@@ -17,36 +15,28 @@ type Props = {
 window.React = React;
 window.ReactDOM = ReactDOM;
 
+class FakeProvider {
+  constructor(public readonly awareness: any) {}
+}
+
 const RichTextRenderer: React.FC<Props> = observer((props) => {
   const { sessionStore } = props;
 
-  const editor = useEditor({
-    onUpdate: ({ editor }) => {
-      console.log(editor.getJSON());
+  const editor = useBlockNote({
+    editorDOMAttributes: {
+      class: styles.editor,
+      "data-test": "editor",
     },
-    editorProps: {
-      attributes: {
-        class: styles.editor,
-        "data-test": "editor",
+    collaboration: {
+      provider: new FakeProvider(props.document.awareness),
+      user: {
+        name: sessionStore.loggedInUserId || "Anonymous",
+        color: sessionStore.userColor,
       },
+      fragment: props.document.data as any,
     },
-    disableHistoryExtension: true,
-    extensions: [
-      CollaborationCursor.configure({
-        provider: {
-          awareness: props.document.awareness,
-        },
-        user: {
-          name: sessionStore.loggedInUserId || "Anonymous",
-          color: sessionStore.userColor,
-        },
-      }),
-      Collaboration.configure({
-        fragment: props.document.data,
-      }),
-    ],
   });
-  return <EditorContent editor={editor} />;
+  return <BlockNoteView editor={editor} />;
 });
 
 export default RichTextRenderer;
