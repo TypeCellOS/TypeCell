153acfd5a1e2dbe4c80f0ef06306927bedcb57dc
yousefed <yousefdardiry@gmail.com>
2023-07-26 10:48:22 +0200

    pwa poc

---
+++ packages/editor/src/sw-tc.ts
@@ -0,0 +1,26 @@
+// PWA config based on https://github.com/vite-pwa/vite-plugin-pwa/tree/main/examples/react-router
+
+import {
+  cleanupOutdatedCaches,
+  createHandlerBoundToURL,
+  precacheAndRoute,
+} from "workbox-precaching";
+import { NavigationRoute, registerRoute } from "workbox-routing";
+
+declare let self: ServiceWorkerGlobalScope;
+
+// @ts-ignore
+self.addEventListener("message", (event: any) => {
+  // @ts-ignore
+  if (event.data && event.data.type === "SKIP_WAITING") self.skipWaiting();
+});
+
+// self.__WB_MANIFEST is default injection point
+
+precacheAndRoute(self.__WB_MANIFEST);
+
+// clean old assets
+cleanupOutdatedCaches();
+
+// to allow work offline
+registerRoute(new NavigationRoute(createHandlerBoundToURL("index.html")));
--- packages/editor/vite.config.ts
+++ packages/editor/vite.config.ts
@@ -1,8 +1,8 @@
 import react from "@vitejs/plugin-react";
 import history from "connect-history-api-fallback";
-import nodePolyfills from "rollup-plugin-polyfill-node";
 import { webpackStats } from "rollup-plugin-webpack-stats";
 import { ViteDevServer } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig } from "vitest/config";
 
 // solves issue that vite dev server doesn't redirect urls with a "." (such as docs/xxx.md) to the SPA fallback. See https://github.com/vitejs/vite/issues/2190
@@ -25,6 +25,48 @@ function redirectAll() {
   };
 }
 
+const pwaOptions: Partial<VitePWAOptions> = {
+  // mode: "development",
+  // base: "/",
+  includeAssets: ["favicon.svg"],
+  manifest: {
+    name: "TypeCell",
+    short_name: "TypeCell",
+    theme_color: "#ffffff",
+    icons: [
+      {
+        src: "pwa-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "pwa-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+      {
+        src: "pwa-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any maskable",
+      },
+    ],
+  },
+  devOptions: {
+    enabled: true,
+    /* when using generateSW the PWA plugin will switch to classic */
+    type: "module",
+    navigateFallback: "index.html",
+  },
+  filename: "sw-tc.ts",
+  srcDir: "src",
+  strategies: "injectManifest",
+  injectManifest: {
+    maximumFileSizeToCacheInBytes: 10000000,
+    globPatterns: ["**/*.{js,css,html,svg,png,ico,gif,woff,woff2}"],
+  },
+};
+
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
@@ -35,7 +77,7 @@ export default defineConfig({
     // global: "globalThis", // breaks some modules work because of https://github.com/vitejs/vite/issues/6295, done in index.tsx instead
     // process & buffer are added to global scope in index.host.tsx
   },
-  plugins: [react(), redirectAll(), webpackStats()],
+  plugins: [react(), webpackStats(), VitePWA(pwaOptions)],
   resolve: {
     // alias: {
     //   buffer: "rollup-plugin-node-polyfills/polyfills/buffer-es6",
@@ -57,7 +99,7 @@ export default defineConfig({
     rollupOptions: {
       // Enable rollup polyfills plugin
       // used during production bundling
-      plugins: [nodePolyfills()],
+      plugins: [],
     },
   },
   test: {
